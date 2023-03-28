d54324166b6b704a76f478f42dfbbf41bd846832
Alexey Leonovich <alexey.leonovich.bel@gmail.com>
2023-03-28 22:49:37 +0300

    completed task 8.1

---
+++ src/main/java/hw_8/Employee.java
@@ -0,0 +1,56 @@
+package hw_8;
+
+public class Employee {
+    //Задача №1
+    //Необходимо создать класс Employee с полями: имя, возраст, пол и ЗП в день.
+    // Все поля сделать приватными и для каждого поля добавить методы set и get.
+    // Класс должен иметь метод - getSalary(int days), метод возвращает зарплату за количество дней которые
+    // были переданы в качестве аргумента.
+    private String name;
+    private int age;
+    private String gender;
+    private double salaryPerDay;
+
+    public Employee(String name, int age, String gender, double salaryPerDay) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salaryPerDay = salaryPerDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public double getSalaryPerDay() {
+        return salaryPerDay;
+    }
+
+    public void setSalaryPerDay(double salaryPerDay) {
+        this.salaryPerDay = salaryPerDay;
+    }
+
+    public double gerSalary(int days) {
+        return days * salaryPerDay;
+    }
+}
---
+++ src/main/java/hw_8/Manager.java
@@ -0,0 +1,30 @@
+package hw_8;
+
+public class Manager extends Employee{
+    //Задача №2
+    //Необходимо создать класс Manager с полями: имя, возраст, пол, ЗП в день и количество подчиненных.
+    // Все поля сделать приватными и для каждого поля добавить методы set и get.
+    // Класс должен иметь метод - getSalary(int days), метод возвращает зарплату за количество дней
+    // которые были переданы в качестве аргумента.
+    //К рассчитанной ЗП должно прибавляться по 1% за каждого подчиненного.
+    //класс Manager расширяет класс  Employee из задачи 1.
+    private int workers;
+
+    public Manager(String name, int age, String gender, double salaryPerDay, int workers) {
+        super(name, age, gender, salaryPerDay);
+        this.workers = workers;
+    }
+
+    public int getWorkers() {
+        return workers;
+    }
+
+    public void setWorkers(int workers) {
+        this.workers = workers;
+    }
+
+    @Override
+    public double gerSalary(int days) {
+       return super.gerSalary(days) * 1.01;
+    }
+}
